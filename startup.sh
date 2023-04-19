9007a32fa94f60ae8919be3310fb74e565ed9989
Chris McDonnell <camcdonn@gmail.com>
2023-04-18 20:35:09 -0400

    Removed old startup script

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-kafka_2.13-3.4.0/bin/zookeeper-server-start.sh kafka_2.13-3.4.0/config/zookeeper.properties &
-kafka_2.13-3.4.0/bin/kafka-server-start.sh kafka_2.13-3.4.0/config/server.properties &
-docker-compose up --build
-
-
